c46af4b83c59ec9670e100b25a0e022703ffe16a
Sittisukintaruk <fah78963214@gmail.com>
2021-04-02 21:01:47 +0700

    delete mockup data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,17 @@
 import "./style/App.css";
 import Header from "./componet/Header";
 import Mainpage from "./componet/Mainpage";
-import { useState, useEffect } from "react";
-import data from "./json/Imgenav.json";
-import dataset from './setData' ; 
+import { useState } from "react";
 
+import dataset from "./setData";
 
 function App() {
- 
-
-  const [imgesicon, setImagesicon] = useState([]);
-  const [videos, setVideos] = useState([]);
   const [datavideo, setDataVideo] = useState(dataset);
   //  ต้องการ ภาพปกของวิดีโอเท่านั้น ซึ่งได้จาก path Search ของ Api
 
-  useEffect(() => {
-    const socialMediaList = data.data;
-    setImagesicon(socialMediaList);
-    // console.log(datavideo);
-  }, []);
-
   return (
     <div className="Container">
-      {imgesicon.length > 0 ? <Header dataimgelish={datavideo} /> : <nav></nav>}
+      {datavideo.length > 0 ? <Header dataimgelish={datavideo} /> : <nav></nav>}
       <Mainpage />
     </div>
   );
