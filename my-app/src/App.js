4a9e46c400785725478c91186f3544043e24b39b
Sittisukintaruk <fah78963214@gmail.com>
2021-04-08 19:33:16 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,12 +6,30 @@ import dataset from "./setData";
 import axios from "axios";
 
 function App() {
+  const [rusia, Aqua, Botan, Kanata] = dataset();
   const [videolish, setVideolish] = useState([]);
-  const [data, setData] = useState(dataset);
+  const [rusiaVideo, setRusiaVideo] = useState(rusia.data);
+
+  useEffect(() => {
+    // const url = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&part=id&channelId=UCK9V2B22uJYu3N7eR_BT9QA&eventType=live&type=video&key=AIzaSyCqmZfh3ujTcbc0m2Y_WLNKnZyQTrun0u8';
+    // await axios(url).then(value => value.data.items.length > 0 ? setRusiaVideo({...rusiaVideo.data,...rusiaVideo.data.videos.videoId = value.data.items[0].id.videoId}):false)
+    // .catch(e => console.log(e));
+    // setRusiaVideo({ ...rusiaVideo, datameged });
+    // setRusiaVideo(newrusia);
+    // console.log(rusiaVideo);
+    const fecth = () => {
+      setRusiaVideo({
+      ...rusia.data,
+      ...rusia.data.videos.videoId = "beer",
+    });
+    console.log(rusiaVideo);
+    }
+    fecth();
+  }, []);
 
   return (
     <div className="Container">
-      <Header dataimgelish={data} />
+      <Header dataimgelish={dataset()} />
       <Mainpage />
     </div>
   );
