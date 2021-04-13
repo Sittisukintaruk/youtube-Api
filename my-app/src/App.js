9d29e4316835974d4785b1b157f613cf18139bc2
Sittisukintaruk <fah78963214@gmail.com>
2021-04-13 14:55:28 +0700

    add env api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,20 +6,18 @@ import dataset from "./json/hololive.json";
 import axios from "axios";
 
 function App() {
-  const apiKey = process.env.YOUTOUBE_API;
   //'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCUKD-uaobj9jiqB-VXt71mA&eventType=live&maxResults=1&order=date&type=video&key=[YOUR_API_KEY]'
-  //!หาก่อนว่า api get ได้มีช่องที่กำลัง live อยู่ 
-  //ถ้ามีการ live จะบันทึกและเก็บไว้ใน array objcet ที่สร้างใหม่  
+  //!หาก่อนว่า api get ได้มีช่องที่กำลัง live อยู่
+  //ถ้ามีการ live จะบันทึกและเก็บไว้ใน array objcet ที่สร้างใหม่
   // array object จะประกอบด้วยรายละเอียดช่องและวิดีโอที่กำลัง live
   useEffect(() => {
+    const apiKey = process.env.REACT_APP_YOUTOUBE_API;
+    const channelID = dataset.hololive[0].channelId;
+    const Url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&eventType=live&maxResults=1&order=date&type=video&key=${apiKey}`;
     console.log(apiKey);
+    console.log(channelID);
+    console.log(Url);
   }, []);
-
-
-  // const Checklive = () => {
-    
-  // } 
-
 
   return (
     <div className="Container">
