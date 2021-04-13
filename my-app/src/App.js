336d578635493dffb67c1288a6bfdcb769e4b786
Sittisukintaruk <fah78963214@gmail.com>
2021-04-13 16:17:30 +0700

    add  data holo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,25 +3,28 @@ import Header from "./componet/Header";
 import Mainpage from "./componet/Mainpage";
 import { useState, useEffect } from "react";
 import dataset from "./json/hololive.json";
-import axios from "axios";
 
 function App() {
   //'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCUKD-uaobj9jiqB-VXt71mA&eventType=live&maxResults=1&order=date&type=video&key=[YOUR_API_KEY]'
   //!หาก่อนว่า api get ได้มีช่องที่กำลัง live อยู่
-  //ถ้ามีการ live จะบันทึกและเก็บไว้ใน array objcet ที่สร้างใหม่
+  //?ถ้ามีการ live จะบันทึกและเก็บไว้ใน array objcet ที่สร้างใหม่
   // array object จะประกอบด้วยรายละเอียดช่องและวิดีโอที่กำลัง live
-  useEffect(() => {
+  const [videos, setVideos] = useState(dataset.hololive);
+
+
+  const checkLive = async () => {
     const apiKey = process.env.REACT_APP_YOUTOUBE_API;
-    const channelID = dataset.hololive[0].channelId;
-    const Url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&eventType=live&maxResults=1&order=date&type=video&key=${apiKey}`;
     console.log(apiKey);
-    console.log(channelID);
-    console.log(Url);
-  }, []);
+  };
+
+  useEffect(() => {
+    checkLive();
+    console.log(videos);
+  }, [videos]);
 
   return (
     <div className="Container">
-      <Header dataimgelish={dataset.hololive} />
+      <Header dataimgelish={videos} />
       <Mainpage />
     </div>
   );
