16bd459b40f953c28958e33d0effb15401bb32ea
Sittisukintaruk <fah78963214@gmail.com>
2021-04-27 20:16:17 +0700

    add video

--- my-app/src/componet/Mainpage.js
+++ my-app/src/componet/Mainpage.js
@@ -1,33 +1,10 @@
 import React from "react";
-import Svgeye from "../img/akar-icons_eye.svg";
-import Svgcolck from "../img/clarity_alarm-clock-line.svg";
+import Video from "./Video"
 
 const Mainpage = () => {
   return (
     <div className="Main">
-      <div className="group-video">
-        <div className="video-card">
-          <div className="video">
-            <img
-              src="https://i.ytimg.com/vi/kXToGY7Bcw8/hqdefault_live.jpg"
-              alt = "ait"
-            />
-          </div>
-          <div className="video-titel">
-            【歌枠】最近暑い！夏！あつあつ夏歌枠☀【白銀ノエル/ホロライブ】
-          </div>
-          <div className="box-view-time">
-            <div className="time">
-              <img src={Svgcolck} alt="clock" />
-              <p>00:19:44</p>
-            </div>
-            <div className="view">
-              <img src={Svgeye} alt="clock" />
-              <p>3000</p>
-            </div>
-          </div>
-        </div>
-      </div>
+     <Video />
     </div>
   );
 };
