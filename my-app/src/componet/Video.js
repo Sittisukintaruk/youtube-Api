3bad9ade94464d28966da475e5707b166c51541a
Sittisukintaruk <fah78963214@gmail.com>
2021-05-06 19:30:18 +0700

    add link video

--- my-app/src/componet/Video.js
+++ my-app/src/componet/Video.js
@@ -2,24 +2,31 @@ import React from "react";
 import Svgeye from "../img/akar-icons_eye.svg";
 import Svgcolck from "../img/clarity_alarm-clock-line.svg";
 
-const Video = ({ thumbnails, title }) => {
+const Video = ({ thumbnails, title, id }) => {
   return (
     <>
-      <div className="video-card">
-        <div className="video">
-          <img src={thumbnails} alt={title} />
-        </div>
-        <div className="video-titel">{title}</div>
-        <div className="box-view-time">
-          <div className="time">
-            <img src={Svgcolck} alt="clock" />
-            <p>00:19:44</p>
+      <div
+        className="video-card"
+        onClick={() => {
+          console.log(id);
+        }}
+      >
+        <a href={`https://www.youtube.com/watch?v=${id}`} target="_blank" style = {{textDecoration : "none"}}>
+          <div className="video">
+            <img src={thumbnails} alt={title} />
           </div>
-          <div className="view">
-            <img src={Svgeye} alt="clock" />
-            <p>3000</p>
+          <div className="video-titel">{title}</div>
+          <div className="box-view-time">
+            <div className="time">
+              <img src={Svgcolck} alt="clock" />
+              <p>00:19:44</p>
+            </div>
+            <div className="view">
+              <img src={Svgeye} alt="clock" />
+              <p>3000</p>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
     </>
   );
